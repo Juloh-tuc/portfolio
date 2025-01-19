5d089b830e5a947ab44df9b6b12862be3f2f5606
Juloh-tuc <julie.tuco@gmail.com>
2025-01-19 16:15:05 +0100

    changement cv

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,6 @@
 import "./Home.css";
 import avatar from "../assets/Moi.png";
-import cv from "../assets/MonCV.pdf";
+import cv from "../assets/MonCv.pdf";
 
 function Home() {
 	return (
@@ -13,7 +13,7 @@ function Home() {
 				</p>
 
 				<div className="avatar-container">
-					<a href={cv} download="MonCV.pdf" className="avatar-link">
+					<a href={cv} download="MonCv.pdf" className="avatar-link">
 						<img src={avatar} alt="Mon avatar" className="avatar" />
 						<span className="download-text">Télécharger mon CV</span>
 					</a>
